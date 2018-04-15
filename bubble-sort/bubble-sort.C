3509e56cf5d42eef67f8794b456041a449facb43
goofy3 <https://github.com/GOOFY3>
2018-04-15 11:34:27 +0530

    bubble-sort

---
+++ bubble-sort/bubble-sort.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void swap(int *, int *);
+
+int main(){
+  int a[10], n, i, j;
+  scanf("%d",&n);
+  for(i=0;i<n;i++){
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++){
+    for(j=0;j<n-i-1;j++){
+      if(a[j]>a[j+1]){
+        swap(&a[j],&a[j+1]);
+      }
+    }
+  }
+  for(i=0;i<n;i++){
+    printf("%d",a[i]);
+  }
+  return 0;
+}
+
+void swap(int *a, int *b){
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
+}
