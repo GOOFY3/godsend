a8489b3710463288739837c2eb0a81fd58282ce7
goofy3 <aquibbaig97@gmail.com>
2018-07-12 21:48:45 +0530

    djikstra algorithm

--- python/Djikstra/djikstra.py
+++ python/Djikstra/djikstra.py
@@ -1,4 +1,5 @@
-import sys;
+import sys
+import heapq
 
 class Vertex(object):
     def __init__(self, name):
@@ -8,25 +9,26 @@ class Vertex(object):
         self.predecessor = None
         self.minDistance = sys.maxsize
     def __cmp__(self, otherVertex):
-        return __cmp__(self.minDistance, otherVertex.minDistance);
+        return cmp(self.minDistance, otherVertex.minDistance);
     def __lt__(self, otherVertex):
         selfPriority = self.minDistance
         otherPriority = otherVertex.minDistance
         return selfPriority < otherPriority
 class Edge(object):
-    def __init__(self, startVertex, targetVertex, weight):
+    def __init__(self, weight, startVertex, targetVertex):
         self.weight = weight
         self.startVertex = startVertex
         self.targetVertex = targetVertex
-    def calculateShortestPath(self, vertexList, targetVertex):
+class Algorithm(object):
+    def calculateShortestPath(self, vertexList, startVertex):
         queue = [];
         startVertex.minDistance = 0
-        heapq.heappush(startVertex)
+        heapq.heappush(queue, startVertex)
         while len(queue) > 0:
             actualVertex = heapq.heappop(queue);
             for edge in actualVertex.adjacentList:
-                u = actualVertex.startVertex
-                v = actualVertex.targetVertex
+                u = edge.startVertex
+                v = edge.targetVertex
                 newDistance = u.minDistance + edge.weight
                 if newDistance < v.minDistance:
                     v.minDistance = newDistance
@@ -38,3 +40,20 @@ class Edge(object):
         while node is not None:
             print("%s -->" %node.name)
             node = node.predecessor
+node1 = Vertex("A")
+node2 = Vertex("B")
+node3 = Vertex("C")
+
+edge1 = Edge(1,node1, node2)
+edge2 = Edge(1,node2, node3)
+edge3 = Edge(node1, node3, 0.1)
+
+node1.adjacentList.append(edge1)
+node1.adjacentList.append(edge2)
+node2.adjacentList.append(edge3)
+
+
+vertexList = {node1, node2, node3}
+algorithm = Algorithm();
+algorithm.calculateShortestPath(vertexList, node1)
+algorithm.getShortestPathTo(node3)
