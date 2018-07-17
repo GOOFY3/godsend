cf31df5ead44b7380ba96e21816cd3f6d0218dc5
goofy3 <aquibbaig97@gmail.com>
2018-07-17 18:01:53 +0530

    do find and algorithm

--- python/Krushkal/krushkal.py
+++ python/Krushkal/krushkal.py
@@ -1,4 +1,4 @@
-class Vertex(object):
+ttclass Vertex(object):
     def __init__(self, name):
         self.name = name
         self.node = None
@@ -15,15 +15,28 @@ class Edge(object):
     def __cmp__(self, other):
         return cmp(self.weight, other.weight)
 class DisjointSets(object):
+    # def __init__(self, vertexList):
+    #     self.vertexList = vertexList
+    #     self.rootNodes = []
+    #     self.nodeCount = 0
+    #     self.setCount = 0
+    #     self.makeSets(vertexList)
+    # def makeSets(vertexList):
+    #     for v in vertexList:
+    #         self.makeSets(v)
+    # def makeSets(self, vertex):
+    #     node = Node(0, len(self.rootNodes), None)
     def __init__(self, vertexList):
         self.vertexList = vertexList
         self.rootNodes = []
         self.nodeCount = 0
         self.setCount = 0
         self.makeSets(vertexList)
-    def makeSets(vertexList):
+    def makeSets(self, vertexList):
         for v in vertexList:
-            self.makeSets(v)
-    def makeSets(self, vertex):
+            self.makeSet(v)
+    def makeSet(self, vertex):
         node = Node(0, len(self.rootNodes), None)
-                
+        vertex.parentNode = node
+        self.nodeCount += 1
+        self.setCount += 1
